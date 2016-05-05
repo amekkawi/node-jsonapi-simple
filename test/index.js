cc60b676e141ba3acdd719480e2d9ad978fe02c2
Andre Mekkawi <github@andremekkawi.com>
2016-05-05 09:35:23 -0400

    Rearrange tests by dependence

--- test/index.js
+++ test/index.js
@@ -66,12 +66,12 @@ expect.extend({
 	}
 });
 
-require('./schema/attributes-object.spec');
 require('./schema/meta-object.spec');
-require('./schema/resource-identifier-object.spec');
-require('./schema/resource-object.spec');
-require('./schema/error-object.spec');
+require('./schema/attributes-object.spec');
 require('./schema/link-object.spec');
 require('./schema/links-object.spec');
 require('./schema/relationship-object.spec');
+require('./schema/resource-identifier-object.spec');
+require('./schema/resource-object.spec');
+require('./schema/error-object.spec');
 require('./schema/document-object.spec');
